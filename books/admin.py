c61a8a55e144672a3f5469970358f8c5535700f5
Ron Rabakukk <ron.rabakukk@gmail.com>
2017-01-25 16:34:00 -0800

    Create custom BookAdmin

--- books/admin.py
+++ books/admin.py
@@ -2,7 +2,25 @@ from django.contrib import admin
 from .models import Book
 from .models import Author
 
-# Register your models here.
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Book Details", {"fields": ["title", "author"]}),
+        ("Review", {"fields": ["is_favorite", "review", "date_reviewed"]})
+    ]
+    
+    readonly_fields = ["date_reviewed"]
 
-admin.site.register(Book)
+    def book_authors(self, obj):
+        return obj.list_authors()
+
+    book_authors.short_description = "Author(s)"
+
+    list_display = ["title", "book_authors", "date_reviewed", "is_favorite"]
+    list_editable = ["is_favorite"]
+    list_display_links = ["title", "date_reviewed"]
+    list_filter = ["is_favorite"]
+    search_fields = ["title", "author__name"]
+
+
 admin.site.register(Author)
--- books/models.py
+++ books/models.py
@@ -13,7 +13,7 @@ class Book(models.Model):
         return "{} by {}".format(self.title, self.list_authors())
 
     def list_authors(self):
-        return "".join([author.name for author in self.authors.all()])
+        return "".join([author.name for author in self.author.all()])
 
     def save(self, *args, **kwargs):
         if (self.review and self.date_reviewed is None):
