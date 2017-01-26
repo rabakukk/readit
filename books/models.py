d626dd6a340f0950bf8a39b24c3ca8f28c36b004
Ron Rabakukk <ron.rabakukk@gmail.com>
2017-01-25 16:02:47 -0800

    Add new Author model, create relationship between Book and Author

--- books/models.py
+++ books/models.py
@@ -3,10 +3,18 @@ from django.db import models
 # Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=150)
-    author = models.CharField(max_length=70, help_text="Use pen name, not real name")
+    author = models.ManyToManyField("Author", related_name="books")
     review = models.TextField(blank=True, null=True)
     date_reviewed = models.DateTimeField(blank=True, null=True)
     is_favorite = models.BooleanField(default=False, verbose_name="Favorite?")
 
     def __str__(self):
         return self.title
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=70, help_text="Use pen name, not real name", unique=True)
+
+    def __str__(self):
+        return self.name
+
